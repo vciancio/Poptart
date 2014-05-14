098499941879a25d5937c1d4db02b44a609ae34e
Christine Rohacz <crohacz@gmail.com>
2014-05-14 13:41:11 -0700

    final sync before moving computers

---
+++ nav.php
@@ -0,0 +1,9 @@
+<?php
+echo '<nav>
+			<p><a href="#CHC" data-role ="button"> Community Health Centers</a>
+			<p><a href="#resources" data-role="button">Resources</a></p>
+			<p><a href="#forums" data-role="button">Forums</a></p>
+			<p><a href="#contact" data-role="button">Contact</a></p>
+			</nav>';
+	?>
+	

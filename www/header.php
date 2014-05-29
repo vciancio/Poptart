14b5e5032600d91ca5fe078de8dc22bea8562927
Christine Rohacz <crohacz@gmail.com>
2014-05-29 16:07:01 -0700

    Update header.php

--- www/header.php
+++ www/header.php
@@ -1,18 +1,24 @@
 <?php
-
-echo '<meta name="format-detection" content="telephone=no" />
-        <meta name="viewport" content="initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
+echo "<!DOCTYPE html>
+<html lang="en" class="ui-mobile">
+    <head>
+        <meta name="format-detection" content="telephone=no" />
+        <!-- WARNING: for iOS 7, remove the width=device-width and height=device-height attributes. See https://issues.apache.org/jira/browse/CB-4323 -->
+        <meta name="viewport" content="height=device-height,width=device-width,initial-scale=1.0,maximum-scale=1.0" >
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="author" content="ChristineRohacz">
 		<meta charset="utf-8" />
 		<title>Poptart</title>
 <!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
-		<link rel="shortcut icon" href="/favicon.ico">
-		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
-        <link rel="stylesheet" type="text/css" href="css/jquery.mobile-1.4.2.css"/>
-		<link rel="stylesheet" type="text/css" href="css/jquery.mobile.icons-1.4.2.css"/>
-		<link rel="stylesheet" type="text/css" href="css/jquery.mobile.theme-1.4.2.css"/>
-		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile.structure-1.4.2.min.css" />
-		<link rel="stylesheet" type="text/css" href="css/index.css" />
-		'; 
-?> 
+		
+		<link rel="stylesheet" type="text/css" href="css/jquery.mobile.flatui.css">
+    	<link rel="stylesheet" type="text/css" href="css/index.css">
+    	<script type="text/javascript" src="phonegap.js"></script>
+    	<script type="text/javascript" src="js/index.js"></script>
+    	<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
+    	<style>
+
+    	</style>
+    </head>";
+?>

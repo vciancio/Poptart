02cd7de36d1b84459a9a7b59e95be3fd60500299
Christine Rohacz <crohacz@gmail.com>
2014-05-29 15:41:20 -0700

    Cleaning up /www
    Eliminating old files that are useless. Added new html pages with
    default theme. Added local jquery library to js folder.

--- www/navbar.php
+++
@@ -1,4 +0,0 @@
-<?php 
-echo '<a href="#mypanel" class="ui-btn-left" id="mygrid" data-role="button" data-icon="grid" data-inline="true" data-iconpos="center"></a>
-        	<a href="#" class="ui-btn-right" data-icon="search" data-role="button" data-inline="true" data-iconpos="center"></a>';
-?>
--- www/panel.php
+++
@@ -1,7 +0,0 @@
-<?php 
-echo '<ul>
-        		<li><a href="loginPage.html" rel="external" data-role ="button">Login</a></li>
-        		<li><a href="locations.html" rel="external" data-role ="button">Locations</a></li>
-        	</ul>';
-?>
-

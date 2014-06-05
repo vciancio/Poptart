ec33aad5f78cbbdb038bdcf765bcc31b26b1cf3e
Christine Rohacz <crohacz@gmail.com>
2014-06-04 18:42:43 -0700

    fixed map

--- www/js/map.js
+++ www/js/map.js
@@ -47,15 +47,7 @@ function success(position) {
       		map: map,
       		title: "marker" 
   		});
-  		
-  		var infowindow = new google.maps.InfoWindow({
-  			content: position.name
-  		}); 
-  		
-  		google.maps.event.addListener(marker, 'click',function() { 
-  			infowindow.open(map,marker); 
-  		}); 
-  } //end function success 
+}
 
 if (navigator.geolocation) {
   		navigator.geolocation.getCurrentPosition(success);
@@ -63,19 +55,37 @@ if (navigator.geolocation) {
   		error('Geo Location is not supported');
 }
 
-google.maps.event.addDomListener(window, 'load', success); */
+google.maps.event.addDomListener(window, 'load', success); 
 
-
-//jQuery mobile maps  
-	$(document).on('pageshow','[data-role=page]',function(){   
-		var mapOptions = {zoom: 5, center: new google.maps.LatLng(34,32), mapTypeId: google.maps.MapTypeId.ROADMAP};
-		var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-		var myMarker1 = new google.maps.Marker({position: new google.maps.LatLng(29.979175, 31.134358), map: map });
-		var myMarker2 = new google.maps.Marker({position: new google.maps.LatLng(32.483333, 44.433333), map: map });
-		var myMarker3 = new google.maps.Marker({position: new google.maps.LatLng(37.95, 27.366667), map: map });
-		var myMarker4 = new google.maps.Marker({position: new google.maps.LatLng(37.638, 21.63), map: map });
-		var myMarker5 = new google.maps.Marker({position: new google.maps.LatLng(37.033333, 27.433333), map: map });
-		var myMarker4 = new google.maps.Marker({position: new google.maps.LatLng(36.433333, 28.216667), map: map });
-		var myMarker5 = new google.maps.Marker({position: new google.maps.LatLng(31.213931, 29.885661), map: map });
+*/
+$(document).on( "pageinit", "#map-page", function() {
+    var defaultLatLng = new google.maps.LatLng(34.0983425, -118.3267434);  // Default to Hollywood, CA when no geolocation support
+    if ( navigator.geolocation ) {
+        function success(pos) {
+            // Location found, show map with these coordinates
+            drawMap(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        }
+        function fail(error) {
+            drawMap(defaultLatLng);  // Failed to find location, show default map
+        }
+        // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
+        navigator.geolocation.getCurrentPosition(success, fail, {maximumAge: 500000, enableHighAccuracy:true, timeout: 6000});
+    } else {
+        drawMap(defaultLatLng);  // No geolocation support, show default map
+    }
+    function drawMap(latlng) {
+        var myOptions = {
+            zoom: 10,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+        // Add an overlay to the map of current lat/lng
+        var marker = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title: "Greetings!"
+        });
+    }
 });
- 
+

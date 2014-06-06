e069e4be66a41befb1e0365a68aa5dcd39732155
Vincente <vciancio@socaldevs.com>
2014-06-06 15:51:30 -0700

    got dynamic markers and info windows working

--- www/js/map.js
+++ www/js/map.js
@@ -17,12 +17,11 @@ function throttle_events(event) {
     };
 };
 
-//custom google maps function
-function success(position) {
-  var coords = new google.maps.LatLng(position.latitude, position.longitude);
+//Display Google Maps
+function displayMap(currentPosition,nodes) {
   var options = {
-    zoom: 8,
-    center: coords,
+    zoom: 15,
+    center: currentPosition,
     mapTypeControl: false,
     navigationControlOptions: {
       style: google.maps.NavigationControlStyle.SMALL
@@ -31,19 +30,49 @@ function success(position) {
   };
 
   var map = new google.maps.Map(document.getElementById("mapcontainer"), options);
-  var marker = new google.maps.Marker({
-    position: coords,
-    map: map,
-    title: "marker"
+//  map.addEventListener("mousemove", throttle_events, true);
+  var lastOpened = -1;
+  var markers = [];
+  $.each(nodes,function(key, node){
+    console.log(node);
+    var coords = new google.maps.LatLng(node.latitude, node.longitude);
+    var marker = new google.maps.Marker({
+      position: coords,
+      map: map,
+      title: node.name
+    });
+    markers.push(marker);
+    var infoContent =
+      '<div id="map-info-window">'+
+        '<div id="siteNotice">'+'</div>'+
+        '<h1>'+node.name+'</h1>'+
+        '<div >'+node.description+'</div>'+
+      '</div>';
+    var infoWindow = new google.maps.InfoWindow({
+      content: infoContent
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      if(lastOpened >= 0 && lastOpened != key){
+        infoWindow.close(map,markers[lastOpened]);
+        alert(markers[lastOpened]);
+      }
+      else if(lastOpened == key){
+        infoWindow.close(map, key);
+        lastOpened = -1;
+      }
+      else{
+        infoWindow.open(map,marker);
+        map.setCenter(marker.position);
+        lastOpened = key;
+      }
+    });
   });
-  map.addEventListener("mousemove", throttle_events, true);
-} //end function success 
 
-if (navigator.geolocation) {
-//  navigator.geolocation.getCurrentPosition(success);
-} else {
-  error('Geo Location is not supported');
-}
+  setTimeout(function() {
+        google.maps.event.trigger(map, 'resize');
+  },500);
+
+} //end function success
 
 $.ajax({
   url: "http://162.243.138.94:6543/api/clinic/get?zipcode=95050&callback=jsonp",
@@ -51,24 +80,31 @@ $.ajax({
   dataType: "jsonp",
   success: function( data ){
       var items = [];
-
+      var nodes = [];
       $.each(data.nodes, function(key, object){
         console.log(object);
+        nodes.push(object);
         $.each( object, function(key, val){
           items.push("<li class=ui-li id='" + key + "'>" + key + ": " + val + "</li>");
-          success(object);
         });
-     });
-     $("<ul/>", {
-     "data-role": "listview",
-    "data-inset": "true", 
+//        success(nodes);
+      });
+      navigator.geolocation.getCurrentPosition(
+        function(position){
+          var myCoords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          displayMap(myCoords, nodes);
+        }
+      );
+      var myCoords = new google.maps.LatLng(37.3492,-121.9381);
+//      displayMap(myCoords, nodes);
+/*      $("<ul/>", {
+        "data-role": "listview",
+        "data-inset": "true", 
         "class":"ui-listview ui-listview-inset",
         "id":"map-list",
         "data-theme": "b",
         html: items.join("")
-     });//.appendTo("body");
-    setTimeout(function() {
-      google.maps.event.trigger(map, 'resize');
-    },500);
+      });//.appendTo("body");
+*/
   }
 });
